a181d0e0fdd84260b6ad79e283c7e3d28b30339b
tungtq123 <he182488toquoctung@gmail.com>
2025-09-20 20:49:54 +0700

    Done bt

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import { useState } from 'react'
 
-function App() {
-  const [counter, setCounter] = useState(1)
 
-  const handleIncrease = () => {
-    setCounter(counter + 1)
+function App() {
+  const [info, setInfo] = useState({
+    name: 'TQT',
+    age: 21,
+    address: 'HD - HN'
+  })
+
+  function updateInfo() {
+   setInfo(
+    {
+      ...info,
+      bio: 'single'
+    }
+   )
   }
 
-  return (
-    <div className="App" style={{padding: 20}}>
-      <h1>{counter}</h1>
-      <button onClick={handleIncrease}>Increase</button>
+
+  return(
+    <div id='root'>
+      <h1>{JSON.stringify(info)}</h1>
+      <button onClick={updateInfo}>
+        Update
+      </button>
     </div>
-  );
+  )
+
 }
 
 export default App;

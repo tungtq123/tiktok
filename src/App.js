e64efe05be1d7d76cde82c7ca7bb4876bb538e9a
tungtq123 <he182488toquoctung@gmail.com>
2025-09-20 23:00:28 +0700

    done

--- src/App.js
+++ src/App.js
@@ -73,6 +73,46 @@ import { useState } from 'react'
 // Ex 4:
 
 // Response from API
+// const courses = [
+//   {
+//     id: 1,
+//     name: 'HTML, CSS'
+//   },
+//   {
+//     id: 2,
+//     name: 'Javascript'
+//   },
+//   {
+//     id: 3,
+//     name: 'ReactJS'
+//   }
+// ]
+
+// function App() {
+//   const [checked, setChecked] = useState(2)
+
+//   const handleSubmit = () => {
+//       console.log({id: checked})
+//   }
+
+//   return (
+//     <div style={{ padding: 32 }}>
+//       {courses.map(course => (
+//         <div key={course.id}>
+//           <input
+//             type="radio"
+//             checked={checked == course.id}
+//             onChange={() => setChecked(course.id)}
+//           />
+//           {course.name}
+//         </div>
+//       ))}   
+
+//       <button onClick={handleSubmit}>Register</button>
+//     </div>
+//   )
+// }
+
 const courses = [
   {
     id: 1,
@@ -89,12 +129,23 @@ const courses = [
 ]
 
 function App() {
-  const [checked, setChecked] = useState()
-
+  const [checked, setChecked] = useState([])
   console.log(checked)
+  const handleCheck = (id) => {
+    setChecked(prev => {
+      const isChecked = checked.includes(id)
+      if(isChecked) {
+        // Uncheck
+        return checked.filter(item => item !== id)
+      }
+      else {
+        return [...prev, id]
+      } 
+    })
+  }
 
   const handleSubmit = () => {
-      
+      console.log({id: checked})
   }
 
   return (
@@ -102,8 +153,9 @@ function App() {
       {courses.map(course => (
         <div key={course.id}>
           <input
-            type="radio"
-            onChange={() => setChecked(course.id)}
+            type="checkbox"
+            checked={checked.includes(course.id)}
+            onChange={() => handleCheck(course.id)}
           />
           {course.name}
         </div>

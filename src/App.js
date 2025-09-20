869246a811ea4079da494e942488d98c12f522db
tungtq123 <he182488toquoctung@gmail.com>
2025-09-20 22:43:09 +0700

    update App.js index.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,117 @@
 import { useState } from 'react'
 
 
-function App() {
-  const [info, setInfo] = useState({
-    name: 'TQT',
-    age: 21,
-    address: 'HD - HN'
-  })
+// Ex 1:
+// function App() {
 
-  function updateInfo() {
-   setInfo(
-    {
-      ...info,
-      bio: 'single'
-    }
-   )
+// const [take, setTake] = useState('Chưa lấy phần thưởng')
+
+// const gifts = [
+//   "CPU 19",
+//   "RAM 32GB RGB",
+//   "RGB Keyboard"
+// ]
+
+// function takeGift () {
+//   setTake(gifts[Math.floor(Math.random() * 3)])
+// }
+
+// return(
+//   <div style={{padding: 32}}>
+//     <h1>{take}</h1>
+//     <button onClick={takeGift}>Lấy phần thưởng</button>
+//   </div>
+// )
+// }
+
+// Ex 2: (Two way binding)
+// function App() {
+//   const [name, setName] = useState('')
+
+//   console.log(name);
+  
+
+//   return(
+//     <div style={{padding: 32}}>
+//       <input 
+//         value={name}
+//         onChange={e => setName(e.target.value)}
+//       />
+//       <button onClick={() => setName('Nguyen Van BBB')}>Change</button>
+//     </div>
+//   )
+// }
+
+// Ex 3:
+// function App() {
+//   const [name, setName] = useState('')
+//   const [email, setEmail] = useState('')
+
+//   const handleSubmit = () => {
+//     // CALL API
+//     console.log({
+//       name, 
+//       email
+//     })
+//   }
+
+//   return(
+//     <div style={{padding: 32}}>
+//       <input 
+//         value={name}
+//         onChange={e => setName(e.target.value)}
+//       />
+//       <input 
+//         value={email}
+//         onChange={e => setEmail(e.target.value)}
+//       />
+//       <button onClick={handleSubmit}>Register</button>
+//     </div>
+//   )
+// }
+
+// Ex 4:
+
+// Response from API
+const courses = [
+  {
+    id: 1,
+    name: 'HTML, CSS'
+  },
+  {
+    id: 2,
+    name: 'Javascript'
+  },
+  {
+    id: 3,
+    name: 'ReactJS'
+  }
+]
+
+function App() {
+  const [checked, setChecked] = useState()
+
+  console.log(checked)
+
+  const handleSubmit = () => {
+      
   }
 
+  return (
+    <div style={{ padding: 32 }}>
+      {courses.map(course => (
+        <div key={course.id}>
+          <input
+            type="radio"
+            onChange={() => setChecked(course.id)}
+          />
+          {course.name}
+        </div>
+      ))}   
 
-  return(
-    <div id='root'>
-      <h1>{JSON.stringify(info)}</h1>
-      <button onClick={updateInfo}>
-        Update
-      </button>
+      <button onClick={handleSubmit}>Register</button>
     </div>
   )
-
 }
 
 export default App;

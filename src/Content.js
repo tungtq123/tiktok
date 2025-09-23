70b6a85580cc5e59bda35d80e45bed7eb2e9e0ed
tungtq123 <he182488toquoctung@gmail.com>
2025-09-24 00:23:52 +0700

    Done

--- src/Content.js
+++ src/Content.js
@@ -10,43 +10,36 @@ import { useEffect, useState } from "react"
 //----------
 // 1. Callback luon duoc goi sau khi component mounted
 
-const tabs = ['posts', 'comments', 'albums']
+const tabs = ['posts', 'comments', 'albums', 'photos', 'todos', 'users']
 
 function Content() {
-    const [title, setTitle] = useState('')
-    const [posts, setPosts] = useState([])
     const [type, setType] = useState('posts')
-
+    const [posts, setPosts] = useState([])
+    
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/${type}`)
         .then(res => res.json())
-        .then(posts =>
+        .then(posts => (
             setPosts(posts)
-        )
+        ))
     }, [type])
-
-    
 
     return(
        <div>
-            {tabs.map(tab => (
-                <button
+            {tabs.map((tab) => (
+                <button 
                     key={tab}
                     style={type === tab ? {
                         color: '#fff',
                         backgroundColor: '#333',
-                    } : {}}
+                    }: {}}
                     onClick={() => setType(tab)}
                 >
                     {tab}
                 </button>
             ))}
-            <input 
-                value={title}
-                onChange={e => setTitle(e.target.value)}
-            />
             <ul>
-                {posts.map(post => (
+                {posts.map((post) => (
                     <li key={post.id}>{post.title || post.name}</li>
                 ))}
             </ul>
